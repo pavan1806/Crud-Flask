3da265ef0df63a1faa44c952190f81cca7969d57
Pavan Chintakayala <pa1chintakayala@gmail.com>
2023-11-24 17:58:12 +0530

    within .py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-# app.py
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template_string, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -20,6 +19,37 @@ def create_table():
         ''')
         connection.commit()
 
+html_code = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task Manager</title>
+</head>
+<body>
+    <h1>Task Manager</h1>
+    <ul>
+        {% for task in tasks %}
+            <li>
+                {{ task[1] }} - {{ task[2] }} - {{ "Completed" if task[3] else "Not Completed" }}
+                <a href="{{ url_for('update', task_id=task[0]) }}">Toggle Status</a>
+                <a href="{{ url_for('delete', task_id=task[0]) }}">Delete</a>
+            </li>
+        {% endfor %}
+    </ul>
+    <form method="post" action="{{ url_for('add') }}">
+        <label for="title">Title:</label>
+        <input type="text" name="title" required>
+        <label for="description">Description:</label>
+        <input type="text" name="description">
+        <button type="submit">Add Task</button>
+    </form>
+</body>
+</html>
+'''
+
 @app.route('/')
 def index():
     create_table()
@@ -27,7 +57,7 @@ def index():
         cursor = connection.cursor()
         cursor.execute('SELECT * FROM tasks;')
         tasks = cursor.fetchall()
-    return render_template('index.html', tasks=tasks)
+    return render_template_string(html_code, tasks=tasks)
 
 @app.route('/add', methods=['POST'])
 def add():
